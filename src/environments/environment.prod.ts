10f32bbf14b56c494eafe2318757d9e397fefc58
Clement Dudit <clement.dudit@etudiant.univ-lr.fr>
2021-02-04 17:06:35 +0100

    init firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD3PyTxGphjJXvrgZ4KMBbb12lng3ytAYQ",
+    authDomain: "mon-petit-terroir.firebaseapp.com",
+    projectId: "mon-petit-terroir",
+    storageBucket: "mon-petit-terroir.appspot.com",
+    messagingSenderId: "183970312512",
+    appId: "1:183970312512:web:ffd67202037ee0c2dd4aa5"
+  }
 };
